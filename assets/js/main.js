eb29b2c60faffb58617db7669e140b13f8ad181a
emabytes <emadavor22@gmail.com>
2020-07-08 20:53:28 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+//input color and change bg color on btn click
+function changeColor() {
+    let newColor = document.getElementById("input-color").value;
+    document.getElementById("section").style.backgroundColor = newColor;
+}
